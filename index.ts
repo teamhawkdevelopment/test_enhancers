43c9e210ef68189d30c35103388872ce473fa197
Hawk Development <team.hawkdevelopment@gmail.com>
2021-04-01 21:55:09 +0200

    Added BodyParser and CORS

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+
+/* Setup the app */
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = 8000;
 
 app.get('/', (req, res) => res.send('It works!'));
+
+/* Run the app */
 
 app.listen(PORT, () => {
     console.log(`Server running at https://localhost:${PORT}`);
